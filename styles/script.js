5aed36821a0c22201e689345a91cd440996d6b7b
briancharlesoreilly <briancharlesoreilly@gmail.com>
2022-03-25 15:54:54 -0400

    javascript for comment section begun. modal styling start.

--- styles/script.js
+++ styles/script.js
@@ -13,3 +13,53 @@ function menuSlide() {
     burgerCheckbox.checked = true;
   }
 }
+
+// listen for user input into any of the input fields on the form
+// -- event listener for when user hits submit button "Post comment"
+// -- when submit button triggers, make sure there is an email input values in all three form fields (email address, name, comment), alert user if there isn't
+// -- popup a choice of avatar for the user to select to associate with the comment post
+
+// store form element 
+const commentForm = document.querySelector("form");
+console.log(commentForm);
+
+// store button element
+const button = commentForm.querySelector("button");
+
+// listen for when button is clicked
+button.addEventListener("click", postComment);
+
+// run this when clicked
+function postComment(click){
+// store name input
+  const userName = document.getElementById("name").value;
+// store email input
+  const userEmail = document.getElementById("email").value;
+// store comment input
+const userComment = document.getElementById("message").value;
+// prevent button click default action
+  click.preventDefault()
+
+  if (userEmail && userComment && userName) {
+    console.log(userName);
+    console.log(userEmail);
+    console.log(userComment);
+  } else {
+    alert(`Please fill in all fields (name, email and comment) to post a comment! Thank you :)`);
+  }
+
+
+
+
+};
+
+
+
+// store user input into a variable(s)
+// -- store user selection of avatar into variable
+// -- store email address, name, and comment input
+// -- generate time and date of the submit button click event, place into a variable
+
+// display user input into appropriate fields in comment section
+// -- create new div's/elements necessary to add to the comment section
+// -- place variables created from user input captured during button submit event trigger into the newly created div/elements and append them to the html
