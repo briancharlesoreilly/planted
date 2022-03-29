6cff81aa7b2c12b86884e8016cfd3f2009f790ce
briancharlesoreilly <briancharlesoreilly@gmail.com>
2022-03-29 09:40:42 -0400

    cleaned up some comments in the code

--- styles/script.js
+++ styles/script.js
@@ -14,34 +14,31 @@ function menuSlide() {
   }
 }
 
+// PSEUDO CODE FOR COMMENT SECTION
 // listen for user input into any of the input fields on the form
 // -- event listener for when user hits submit button "Post comment"
 // -- when submit button triggers, make sure there is an email input values in all three form fields (email address, name, comment), alert user if there isn't
 // -- popup a choice of avatar for the user to select to associate with the comment post
+// store user input into a variable(s)
+// -- store user selection of avatar into variable
+// -- store email address, name, and comment input
+// -- generate time and date of the submit button click event, place into a variable
+// display user input into appropriate fields in comment section
+// -- create new div's/elements necessary to add to the comment section
+// -- place variables created from user input captured during button submit event trigger into the newly created div/elements and append them to the html
 
-// store form element 
+// store form, button, div elements, etc
 const commentForm = document.querySelector("form");
-
-// store button elements
 const button = commentForm.querySelector("button");
-
-// store avatar div
 const avatarModal = document.querySelector(".avatar");
-
-// store #comment-section-flex as an element
 const commentSection = document.getElementById("comment-section-flex");
-
-
-// store instructions class in a variable
 const instructions = document.getElementById("instructions");
-// store avatar choices as variables
 const pokemon1 = document.getElementById("pokemon1"); 
 const pokemon2 = document.getElementById("pokemon2");
 const pokemon3 = document.getElementById("pokemon3");
 const pokemon4 = document.getElementById("pokemon4");
 const pokemon5 = document.getElementById("pokemon5");
 const pokemon6 = document.getElementById("pokemon6");
-
 
 // listen for when "post comment button is clicked
 button.addEventListener("click", postComment);
@@ -59,9 +56,8 @@ function postComment(click){
   function saveAvatarChoice(pokemon) {
     avatarModal.style.display = "none";
     const avatarPic = pokemon.target.parentElement.firstElementChild.outerHTML;
-  
-    console.log(avatarPic);
-  
+
+
     // create new comment function
     function addComment () {
     // insert HTML into comment section with all user generated info
@@ -81,9 +77,6 @@ function postComment(click){
 
   // run program so long as the user inputs values for email, name, AND a comment (all three required), otherwise alert them to do so.
   if (userEmail && userComment && userName) {
-    console.log(userName);
-    console.log(userEmail);
-    console.log(userComment);
     // popup modal to choose avatar
     avatarModal.style.display = "flex";
 
@@ -102,30 +95,18 @@ function postComment(click){
       pokemon6.addEventListener("click", saveAvatarChoice);
     }
 
-
-    // BELOW IS WHAT YOUR WORKING ON LAST AND IT DOESNT WORK. IT DOESNT CLEAR ANYTHING BRIAN
-
     // clear input fields to empty strings when submitted
+    // *****not working**********
     userName.value = "";
     userEmail.value = "";
     userComment.value = "";
-
-    // UP IS WHAT YOUR WORKING ON LAST AND IT DOESNT WORK. IT DOESNT CLEAR ANYTHING BRIAN
-
+    // *****not working**********
 
   } else {
     alert(`Please fill in all fields (name, email and comment) to post a comment! Thank you :)`);
   }
-  
 };
 
-
-
-// store user input into a variable(s)
-// -- store user selection of avatar into variable
-// -- store email address, name, and comment input
-// -- generate time and date of the submit button click event, place into a variable
-
-// display user input into appropriate fields in comment section
-// -- create new div's/elements necessary to add to the comment section
-// -- place variables created from user input captured during button submit event trigger into the newly created div/elements and append them to the html
+// note: things I didn't get to finish, or couldn't figure out
+// 1) clear information from forms after click
+// 2) find a way to generate the date and time when a button was submitted and store it in a variable to publish on the form
